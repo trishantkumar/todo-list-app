85eab72b39256a238cb83d27bce3fa03a1118228
Trishant Kumar <trishantkumar@Trishants-MacBook.local>
2023-11-09 13:50:40 +0530

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,122 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="main">
+    <div class="container">
+      <div class="heading">
+        <div>ToDo App</div>
+      </div>
+      <div class="add_todo_container">
+        <div class="todo_label">Add ToDo</div>
+        <div class="add_todo">
+          <input type="text" class="" v-model="newTodoText" placeholder="Add ToDo"/>
+        </div>
+        <button @click="addTodo()">Add New ToDo</button>
+      </div>
+      <div class="todo_list_container">
+        <div class="list_lable">List of ToDo:</div>
+        <ul class="todo" v-if="todoList && todoList.length">
+          <li class="todo_inner" v-for="(todo, index) in todoList" :key="index">
+            <div>• {{ todo }}</div>
+            <div class="remove_button" @click="removeTodo(index)">&#x2716;</div>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+<script setup>
+import { ref } from 'vue';
+const todoList = ref([])
+const newTodoText = ref('')
+
+function addTodo() {
+  todoList.value.push(newTodoText.value);
+  newTodoText.value = '';
+}
+
+function removeTodo(index) {
+  todoList.value.splice(index, 1);
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style lang="scss">
+.main {
+  color: #fff;
+  font-family: Arial, Helvetica, sans-serif;
+  margin-top: 2rem;
+  box-sizing: border-box !important;
+  .container {
+    width: 600px;
+    margin: auto;
+    .heading {
+      font-weight: bold;
+      font-size: 3rem;
+    }
+    .add_todo_container {
+      margin-top: 2rem;
+      .todo_label {
+        font-weight: 600;
+        font-size: 1.5rem;
+        text-align: left;
+      }
+      .add_todo {
+        padding-top: 0.5rem;
+        input {
+          width: 100%;
+          padding: 16px 20px;
+          border: none;
+          border-radius: 4px;
+          background-color: #f1f1f1;
+          box-sizing: border-box;
+        }
+        input[type=text]:focus {
+          border: none;
+        }
+      }
+      button {
+        background: #645bff;
+        border: 2px solid white;
+        color: white;
+        padding: 20px;
+        font-weight: 600px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin-top: 1rem;
+        cursor: pointer;
+        border-radius: 8px;
+        width: 100%;
+      }
+    }
+    .todo_list_container {
+      background-color: #fff;
+      color: black;
+      border-radius: 8px;
+      padding: 1rem;
+      margin-top: 2rem;
+      .list_lable {
+        font-weight: 600;
+        font-size: 1.5rem;
+      }
+      .todo {
+        text-align: left;
+        margin-top: 1rem;
+        padding: 0 5px;
+        .todo_inner {
+          display: flex;
+          justify-content: space-between;
+          border-bottom: 1px solid #e0e0e0;
+          padding: 10px 0;
+          .remove_button {
+            font-weight: bold;
+            font-size: 20px;
+            cursor: pointer;
+            color: #3b71ca;
+          }
+        }
+      }
+    }
+  }
 }
 </style>
